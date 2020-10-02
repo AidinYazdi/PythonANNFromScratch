37edda383f23a78265c3792ef93c66e7d4ba5f8a
Aidin Yazdi <aidinyazdi@gmail.com>
2020-10-02 13:52:21 +0300

    Cleaned up the file structure to allow for multiple functions. Function2 takes a (1, 10) array where all the elements are 0 accept one element which is 1 and the ANN outputs the exact same array. After some testing, I have decided that approximating y=sin(x) isn't feasible with my computing power and the sigmoid activation function. In the future, I may replace the sigmoid activation function with ReLU in order to make the ANN better for non-binary functions (like y=sin(x)). Since the handwritten digits problem is binary, this will (probably) be the final commit before I branch off for the handwritten digits problem

--- ann.py
+++ ann.py
@@ -1,12 +1,16 @@
 # import required libraries
 import numpy as np
-import function1
+import function2 as functionx
+
+# IMPORTANT: change if approximating a different function
+# the function we are approximating
+func = "function2"
 
 # how many nodes in each layer
-nodes_in_input_layer = 2
-nodes_in_hidden_layer1 = 3
-nodes_in_hidden_layer2 = 3
-nodes_in_output_layer = 1
+nodes_in_input_layer = functionx.input_nodes
+nodes_in_hidden_layer1 = functionx.hidden1_nodes
+nodes_in_hidden_layer2 = functionx.hidden2_nodes
+nodes_in_output_layer = functionx.output_nodes
 
 # whether or not the ANN should read in data
 read_in_data = True
@@ -23,9 +27,6 @@ display = True
 # whether or not the ANN should test itself
 test = True
 
-# the function we are approximating
-func = "function1"
-
 # how many epochs the program should do
 num_epochs = 200000
 
@@ -37,12 +38,12 @@ progress_rate = 10000
 lr = 0.025
 
 # getting the input features from the .csv file
-input_features = np.loadtxt(func + '_inputs.csv', delimiter=',', ndmin=function1.input_array_dim)
-input_features = input_features.reshape(function1.input_array_r, function1.input_array_c)
+input_features = np.loadtxt(func + '/' + func + '_inputs.csv', delimiter=',', ndmin=functionx.input_array_dim)
+input_features = input_features.reshape(functionx.input_array_r, functionx.input_array_c)
 
 # getting the target outputs from the .csv file
-target_output = np.loadtxt(func + '_desired_outputs.csv', delimiter=',', ndmin=function1.output_array_dim)
-target_output = target_output.reshape(function1.output_array_r, function1.output_array_c)
+target_output = np.loadtxt(func + '/' + func + '_desired_outputs.csv', delimiter=',', ndmin=functionx.output_array_dim)
+target_output = target_output.reshape(functionx.output_array_r, functionx.output_array_c)
 
 # setting up the weights and biases
 # reading in the weights and biases
@@ -52,20 +53,20 @@ if read_in_data:
         dim = 2
     else:
         dim = 1
-    weight_hidden1 = np.loadtxt('weight_hidden1.csv', delimiter=',', ndmin=dim)
+    weight_hidden1 = np.loadtxt(func + '/' + 'weight_hidden1.csv', delimiter=',', ndmin=dim)
     if (nodes_in_hidden_layer1 * nodes_in_hidden_layer2) > 1:
         dim = 2
     else:
         dim = 1
-    weight_hidden2 = np.loadtxt('weight_hidden2.csv', delimiter=',', ndmin=dim)
+    weight_hidden2 = np.loadtxt(func + '/' + 'weight_hidden2.csv', delimiter=',', ndmin=dim)
     if (nodes_in_hidden_layer2 * nodes_in_output_layer) > 1:
         dim = 2
     else:
         dim = 1
-    weight_output = np.loadtxt('weight_output.csv', delimiter=',', ndmin=dim)
-    bias1 = np.loadtxt('bias1.csv', delimiter=',', ndmin=1)
-    bias2 = np.loadtxt('bias2.csv', delimiter=',', ndmin=1)
-    bias_op = np.loadtxt('bias_op.csv', delimiter=',', ndmin=1)
+    weight_output = np.loadtxt(func + '/' + 'weight_output.csv', delimiter=',', ndmin=dim)
+    bias1 = np.loadtxt(func + '/' + 'bias1.csv', delimiter=',', ndmin=1)
+    bias2 = np.loadtxt(func + '/' + 'bias2.csv', delimiter=',', ndmin=1)
+    bias_op = np.loadtxt(func + '/' + 'bias_op.csv', delimiter=',', ndmin=1)
 else:
     # randomly assigning the weights and biases in we are not
     # supposed to read them in from the .csv files
@@ -75,6 +76,16 @@ else:
     bias1 = np.random.rand(nodes_in_hidden_layer1)
     bias2 = np.random.rand(nodes_in_hidden_layer2)
     bias_op = np.random.rand(nodes_in_output_layer)
+
+
+# to normalize the input and output data
+def normalize(input_arr, output_arr):
+    input_arr = (input_arr - functionx.input_normalization_min) / (functionx.input_normalization_max - functionx.input_normalization_min)
+    output_arr = (output_arr - functionx.output_normalization_min) / (functionx.output_normalization_max - functionx.output_normalization_min)
+
+
+# do the actual normalization
+normalize(input_features, target_output)
 
 
 # the sigmoid function
@@ -88,10 +99,12 @@ def sigmoid_der(x):
 
 
 # forward propagation
-def forward_prop(x, y):
-    # predicting for (x,y)
+def forward_prop(arr):
+    # normalize the input values
+    arr = (arr - functionx.input_normalization_min) / (functionx.input_normalization_max - functionx.input_normalization_min)
+    # predicting for the values
     # taking the inputs
-    single_point = np.array([x, y])
+    single_point = np.array(arr)
     # first step
     result1 = np.dot(single_point, weight_hidden1) + bias1
     # second step
@@ -104,6 +117,9 @@ def forward_prop(x, y):
     result5 = np.dot(result4, weight_output) + bias_op
     # sixth step
     result6 = sigmoid(result5)
+    # denormalize everything after the calculations have been done
+    single_point = (single_point * (functionx.input_normalization_max - functionx.input_normalization_min)) + functionx.input_normalization_min
+    result6 = (result6 * (functionx.output_normalization_max - functionx.output_normalization_min)) + functionx.output_normalization_min
     # print the final result
     print("inputs:")
     print(single_point)
@@ -183,8 +199,9 @@ if train:
 
         # derivatives for the hidden layer1
         # derror/dino2 and douto/douth2 have been defined above
-        # dino/douth2 needs to be transposed to make it a compatable matrix
-        derror_dino1 = derror_dino2 * dino_douth2.T
+        # dino/douth2 needs to be transposed to make it a compatible matrix
+        # derror_dino1 = derror_dino2 * dino_douth2.T
+        derror_dino1 = np.dot(derror_dino2, dino_douth2.T)
         dino_douth1 = weight_hidden2
         # combine the two previous derivatives into derror/douth2
         # we have to transpose the dino/douth1 to make it compatible with
@@ -216,12 +233,12 @@ if train:
 # # save all the ANN data
 # save_data(save)
 if save:
-    np.savetxt('weight_hidden1.csv', weight_hidden1, delimiter=',')
-    np.savetxt('weight_hidden2.csv', weight_hidden2, delimiter=',')
-    np.savetxt('weight_output.csv', weight_output, delimiter=',')
-    np.savetxt('bias1.csv', bias1, delimiter=',')
-    np.savetxt('bias2.csv', bias2, delimiter=',')
-    np.savetxt('bias_op.csv', bias_op, delimiter=',')
+    np.savetxt(func + '/' + 'weight_hidden1.csv', weight_hidden1, delimiter=',')
+    np.savetxt(func + '/' + 'weight_hidden2.csv', weight_hidden2, delimiter=',')
+    np.savetxt(func + '/' + 'weight_output.csv', weight_output, delimiter=',')
+    np.savetxt(func + '/' + 'bias1.csv', bias1, delimiter=',')
+    np.savetxt(func + '/' + 'bias2.csv', bias2, delimiter=',')
+    np.savetxt(func + '/' + 'bias_op.csv', bias_op, delimiter=',')
     print("the ANN has been saved")
 
 if display:
@@ -240,7 +257,15 @@ if display:
     print(bias_op)
 
 if test:
-    forward_prop(0, 0)
-    forward_prop(1, 0)
-    forward_prop(0, 1)
-    forward_prop(1, 1)
+    if func == "function1":
+        forward_arr = np.array([0, 0], dtype=np.float64)
+        forward_prop(forward_arr)
+        forward_arr = np.array([1, 0], dtype=np.float64)
+        forward_prop(forward_arr)
+        forward_arr = np.array([0, 1], dtype=np.float64)
+        forward_prop(forward_arr)
+        forward_arr = np.array([1, 1], dtype=np.float64)
+        forward_prop(forward_arr)
+    elif func == "function2":
+        forward_arr = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float64)
+        forward_prop(forward_arr)
